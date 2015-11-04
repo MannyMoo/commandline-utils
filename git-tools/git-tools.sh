f40eb599a89e9c009f9fa20ff255017a9bf28bbc
Michael Alexander <michael.alexander@glasgow.ac.uk>
2015-11-04 15:05:00 +0000

    Fixing editing of ssh config.

--- git-tools/git-tools.sh
+++ git-tools/git-tools.sh
@@ -4,24 +4,31 @@ function gen_git_key() {
     if [ ! -e ~/.ssh ] ; then
 	mkdir ~/.ssh
     fi
-    local keyname=~/.ssh/id_rsa.git
+    local keyname='~/.ssh/id_rsa.git'
     ssh-keygen -t rsa -b 4096 -f "$keyname" -C "$(git config --get user.email)"
     if [ -z `eval "$(ssh-agent -s)"` ] ; then
 	echo "ssh agent isn't running! Can't add git id."
     else
 	ssh-add "$keyname"
     fi
-    if [ ! -e ~/.ssh/config ] ; then 
-	touch ~/.ssh/config
-    fi
-    local configlines=`sed '1 i\
-Host github.com\
-\   PubkeyAuthentication yes\
-\   IdentityFile '"${keyname}"'\
-' ~/.ssh/config`
-    echo "$configlines" > ~/.ssh/config
     echo "Public key:"
     cat "${keyname}.pub"
+    set_git_ssh_config
+}
+
+function set_git_ssh_config() {
+    local keyname='~/.ssh/id_rsa.git'
+    if [ ! -e ~/.ssh/config ] ; then 
+	touch ~/.ssh/config
+    else
+	cp ~/.ssh/config ~/.ssh/config.bkup
+    fi
+    # Could use sed -i but requires sed -i "" on MacOS.
+    sed "1 i\\
+Host github.com\\
+\   PubkeyAuthentication yes\\
+\   IdentityFile ${keyname}\\
+" ~/.ssh/config > ~/.ssh/config.tmp && mv ~/.ssh/config.tmp ~/.ssh/config
 }
 
 function test_git_ssh() {
