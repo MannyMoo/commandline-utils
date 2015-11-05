a56161db6cada21e740dafe19ab42225c9fe35e0
Michael Alexander <michael.alexander@glasgow.ac.uk>
2015-11-05 09:55:06 +0000

    Fixing source-all

--- source-all.sh
+++ source-all.sh
@@ -3,7 +3,10 @@ function source_all() {
     if [ ! -z "$1" ] ; then 
 	local d="$1"
     else
-	local d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	# If you call this within a script this just finds the directory
+	# of the parent script. 
+	#local d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	local d='.'
     fi
     for f in `find "$d" -mindepth 1 -name "*.sh"` ; do
 	#echo $f
