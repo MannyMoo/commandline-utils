8a2a1184262fb9141dc4fb08da804d62aa429799
malexand <michael.alexander@glasgow.ac.uk>
2017-02-10 13:40:08 +0100

    Adding option to specify content for the bash script rather than edit it

--- shell-tools/bash-tools.sh
+++ shell-tools/bash-tools.sh
@@ -6,10 +6,15 @@ alias fresh-bash="env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER PATH=/
 alias fresh-bash-login="fresh-bash --login"
 
 function bash-script() {
-    # Create a new executable bash script with the given name and edit it. 
+    # Create a new executable bash script with the given name and either
+    # fill it with commands or edit it. 
     local fname=$1
     echo "#!/bin/bash
 " > $fname
     chmod +x $fname
-    eval "$EDITOR $fname"
+    if [ ! -z $2 ] ; then
+	echo $2 >> $fname
+    else 
+	eval "$EDITOR $fname"
+    fi
 }
