c81a248ec5d8210c78c46113c7aa5b040b4ccbb4
malexand <michael.alexander@glasgow.ac.uk>
2017-02-10 13:35:59 +0100

    Fixing fresh-bash alias; adding function to create and edit a new executable bash script.

--- shell-tools/bash-tools.sh
+++ shell-tools/bash-tools.sh
@@ -2,5 +2,14 @@
 
 # Tools for bash. 
 
-alias fresh-bash="env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER bash"
+alias fresh-bash="env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER PATH=/usr/sue/bin:/bin:/usr/bin:/usr/sbin:/sbin bash"
 alias fresh-bash-login="fresh-bash --login"
+
+function bash-script() {
+    # Create a new executable bash script with the given name and edit it. 
+    local fname=$1
+    echo "#!/bin/bash
+" > $fname
+    chmod +x $fname
+    eval "$EDITOR $fname"
+}
