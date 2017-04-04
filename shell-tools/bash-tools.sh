15a57244159d9213208dbe482f6f13d30cf4dec6
malexand <michael.alexander@glasgow.ac.uk>
2017-04-04 12:04:33 +0200

    Update script creating methods to be generic for any shell; add python-script method.

--- shell-tools/bash-tools.sh
+++ shell-tools/bash-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Tools for bash. 
+source $COMMANDLINEUTILSROOT/shell-tools/script-tools.sh
 
 alias fresh-bash="env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER PATH=/usr/sue/bin:/bin:/usr/bin:/usr/sbin:/sbin bash"
 alias fresh-bash-login="fresh-bash --login"
@@ -8,13 +9,5 @@ alias fresh-bash-login="fresh-bash --login"
 function bash-script() {
     # Create a new executable bash script with the given name and either
     # fill it with commands or edit it. 
-    local fname=$1
-    echo "#!/bin/bash
-" > $fname
-    chmod +x $fname
-    if [ ! -z $2 ] ; then
-	echo $2 >> $fname
-    else 
-	eval "$EDITOR $fname"
-    fi
+    exe-script bash $1 $2
 }
---
+++ shell-tools/script-tools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function exe-script() {
+    # Create a new executable script with the given name and shell 
+    # and either fill it with commands or edit it. 
+    local shell=$1
+    local fname=$2
+    if [ "python" = "$shell" ] ; then
+	echo "#!/bin/env python
+" > $fname
+    else 
+	echo "#!$(which $shell)
+" > $fname
+    fi
+    chmod +x $fname
+    if [ ! -z $3 ] ; then
+	echo $3 >> $fname
+    else 
+	eval "$EDITOR $fname"
+    fi
+}
+
+function python-script() {
+    exe-script python $1 $2
+}
