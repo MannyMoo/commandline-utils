1ff361e64d8711eed116f56afd4d27ee58ec71cf
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2020-07-16 11:10:05 +0100

    Improve fresh-bash functions; add tmux aliases

--- shell-tools/bash-tools.sh
+++ shell-tools/bash-tools.sh
@@ -3,8 +3,13 @@
 # Tools for bash. 
 source $COMMANDLINEUTILSROOT/shell-tools/script-tools.sh
 
-alias fresh-bash="env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER PATH=/usr/sue/bin:/bin:/usr/bin:/usr/sbin:/sbin bash"
-alias fresh-bash-login="fresh-bash --login"
+function fresh-bash() {
+    env -i SHELL=/bin/bash HOME=$HOME TERM=$TERM USER=$USER PATH=/usr/sue/bin:/bin:/usr/bin:/usr/sbin:/sbin bash $@
+}
+
+function fresh-bash-login(){
+    fresh-bash --login $@
+}
 
 function bash-script() {
     # Create a new executable bash script with the given name and either
---
+++ tmux-tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Aliases for tmux
+
+alias tn="tmux new-session -s"
+alias ta="tmux attach -t"
