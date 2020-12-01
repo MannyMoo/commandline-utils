4632a49995a5a1368908283a034b9b12a579e9d6
Michael Alexander <michael.alexander@glasgow.ac.uk>
2020-12-01 17:41:08 +0000

    Fixes

--- image-tools.sh
+++ image-tools.sh
@@ -4,13 +4,13 @@ function img-creation-date() {
     # Get the creation date of an image. Must have either identify or exiftool installed
     img=$1
     if [ ! -z "$(command -v identify)" ] ; then
-	info="$(identify -verbose $img | grep 'Create \?Date')"
+	info="$(identify -verbose $img)"
     elif [ ! -z "$(command -v exiftool)" ] ; then
-	info="$(exiftool DSC_0318.JPG | grep 'Create \?Date' | tail -n 1 | sed 's/T/ /')"
+	info="$(exiftool DSC_0318.JPG)"
     else
 	echo "Requires identify or exiftool!"
     fi
-
-    cdate="$(echo ${info} | sed 's/Create.*Date[ ]*: //')"
+    info="$(echo ${info} | grep 'Create \?Date' | tail -n 1 | sed 's/T/ /')"
+    cdate="$(echo ${info} | awk '{print $(NF-1), $NF}')"
     echo "$cdate"
 }
