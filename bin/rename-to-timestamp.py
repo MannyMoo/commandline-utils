2dd79b02cce4ed16a1eccea4c59da3da8e17c257
MannyMoo <airstream.driver@gmail.com>
2021-10-14 12:49:03 +0100

    Working version

--- bin/rename-to-timestamp.py
+++ bin/rename-to-timestamp.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
 '''Rename files to their EXIF timestamp.'''
+from __future__ import print_function
+import subprocess
+import shutil
+import argparse
+import os
+import sys
+# import click
 
-import subprocess, shutil, argparse, os
+# @click.group()
+# def main():
+#     pass
 
-def get_timestamp(fname) :
+
+def get_timestamp_identify(fname) :
     '''Get the EXIF timestamp from a photo file. Requires Imagemagick for identify.'''
     proc = subprocess.Popen(['identify', '-verbose', fname], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     stdout, stderr = proc.communicate()
@@ -14,73 +24,111 @@ def get_timestamp(fname) :
     dline = filter(lambda l : 'DateTime' in l, stdout.splitlines())[0]
     return ' '.join(dline.split()[-2:])
 
-def rename(fname, newname) :
-    '''Rename a file.'''
-    # Try using shutil, if that fails for whatever reason try using system mv.
-    try :
-        shutil.move(fname, newname)
-    except :
-        result = subprocess.call(['mv', fname, newname])
-        if 0 != result :
-            raise OSError("Couldn't rename " + fname + " to " + newname + "!")
 
-def rename_to_timestamp(fname, dryrun = False, prevrenames = ()) :
-    '''Rename the file to its EXIF timestamp.'''
+def get_timestamp_exif(fname):
+    '''Get the EXIF timestamp from a photo file. Requires exif commandline tool.'''
+    args = ['exif', '-m', fname]
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if 0 != proc.poll():
+        raise OSError('Failed to call {0!r} (requires exif to be installed)\n'.format(' '.join(args))
+                      + stderr)
+    stdout = stdout.splitlines()
+    date = ''
+    dline = list(filter(lambda l : l.startswith('Date and Time'), stdout))
+    if dline:
+        date += dline[0].split('\t')[-1].strip()
+    msline = list(filter(lambda l : l.startswith('Sub-second Time'), stdout))
+    if msline:
+        ms = msline[0].split('\t')[-1].strip()
+        date += '.' + ms.zfill(3)
+    return date
 
-    date = get_timestamp(fname)
-    dirname = os.path.dirname(fname)
-    datename = os.path.join(dirname, date.replace(' ', '_').replace(':', ''))
-    filetype = '.' + fname.split('.')[-1].lower()
-    newname = datename + filetype
-    if newname == fname :
-        if dryrun :
-            print fname, '->', newname
-        return newname
+
+def md5sum(fname):
+    '''Get the md5sum of the file.'''
+    args = ['md5sum', fname]
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if 0 != proc.poll():
+        raise OSError('Failed to call {0!r} (requires md5sum to be installed)\n'.format(' '.join(args))
+                      + stderr)
+    return stdout.split()[0]
+
+
+class Renamer(object):
+    '''File renamer.'''
+    def __init__(self, dryrun=False, outputdir=None, logs=(sys.stdout,),
+                 get_timestamp=get_timestamp_exif):
+        self.dryrun = dryrun
+        self.outputdir = outputdir
+        self.logs = logs
+        self.get_timestamp = get_timestamp
+        
+    def _rename(self, fname, newname):
+        '''Rename a file.'''
+        for f in self.logs:
+            print(fname, '->', newname, file=f)
+        if self.dryrun or fname == newname:
+            return
     
-    # Check if a file of that name exists, or would exist in the case of a dry run.
-    if os.path.exists(newname) or newname in prevrenames :
-        i = 1
-        newname = datename + '_' + str(i) + filetype
-        while os.path.exists(newname) or newname in prevrenames :
-            i += 1
-            newname = datename + '_' + str(i) + filetype
-    if dryrun :
-        print fname, '->', newname
+        # Try using shutil, if that fails for whatever reason try using system mv.
+        try :
+            shutil.move(fname, newname)
+        except :
+            result = subprocess.call(['mv', fname, newname])
+            if 0 != result :
+                raise OSError("Couldn't rename " + fname + " to " + newname + "!")
+
+    def output_name(self, fname):
+        '''Get the output name of a file.'''
+        date = self.get_timestamp(fname)
+        date = date.replace(' ', '_').replace(':', '-')
+        if not self.outputdir:
+            outputdir = os.path.dirname(fname)
+        else:
+            outputdir = self.outputdir
+        datename = os.path.join(outputdir, date)
+        md5 = md5sum(fname)
+        filetype = '.' + fname.split('.')[-1].lower()
+        newname = datename + '_' + md5 + filetype
         return newname
 
-    rename(fname, newname)
-    return newname
+    def rename(self, fname):
+        '''Rename the file to its EXIF timestamp.'''
+        newname = self.output_name(fname)
+        self._rename(fname, newname)
+        return newname
+
 
 def main() :
     '''Main method.'''
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--dryrun', '-n', action = 'store_true',
                            help = "Don't rename anything, just print what would be renamed")
+    argparser.add_argument('--outputdir', '-o', default=None)
+    argparser.add_argument('--logfile', '-f', default=None)
     argparser.add_argument('files', nargs = '+', help = 'Files to rename.')
 
     args = argparser.parse_args()
 
-    renames = []
-    for fname in args.files :
-        renames.append(rename_to_timestamp(fname, dryrun = args.dryrun, prevrenames = renames))
-
-    # For any photos with the same timestamp, and a _0 suffix to the first one, so they're correctly ordered
-    # alphabetically.
-    # There might be a tidier way to do this.
-    for fname in filter(lambda f : f.split('.')[-2].endswith('_1'), renames) :
-        # Remove the _1.
-        name = '.'.join(fname.split('.')[:-1])[:-2]
-        filetype = '.' + fname.split('.')[-1]
-        newname = name + '_0' + filetype
-        name += filetype
-        if os.path.exists(newname) :
-            raise OSError("Can't rename " + name + " -> " + newname + " as a file named " + newname \
-                          + " already exists!")
-
-        if args.dryrun :
-            print name, '->', newname
-        else :
-            rename(name, newname)
+    if args.outputdir and not os.path.exists(args.outputdir):
+        os.makedirs(args.outputdir)
         
+    logs = [sys.stdout,]
+    if args.logfile:
+        fout = open(args.logfile, 'w')
+        logs.append(fout)
+
+    renamer = Renamer(dryrun=args.dryrun, outputdir=args.outputdir,
+                      logs=logs)
+    for fname in args.files :
+        renamer.rename(fname)
+        
+    if args.logfile:
+        fout.close()
+
+
 if __name__ == '__main__' :
     main()
