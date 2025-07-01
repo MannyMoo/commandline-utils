6cc465bcd67ebb787aaba61bdac00da3e262345e
michael.alexander <michael.alexander@satalia.com>
2025-07-01 11:51:44 +0100

    Feature: modify format-json.py to output to stdout or edit in place

--- bin/format-json.py
+++ bin/format-json.py
@@ -6,10 +6,15 @@ import json
 parser = ArgumentParser("format-json.py")
 parser.add_argument("filename", help="File to format")
 parser.add_argument("--indent", default=2, help="Indent for formatting")
-parser.add_argument("--output", default=None, help="Output file name. If not given, overwrite the input file")
+parser.add_argument(
+    "-i", "--in-place", action="store_true", help="Whether to edit in place & overwrite the existing file",
+)
 
 args = parser.parse_args()
 with open(args.filename) as f:
     data = json.load(f)
-with open(args.output if args.output else args.filename, "w") as f:
-    json.dump(data, f, indent=args.indent)
+if args.in_place:
+    with open(args.filename, "w") as f:
+        json.dump(data, f, indent=args.indent)
+else:
+    print(json.dumps(data, indent=args.indent))
